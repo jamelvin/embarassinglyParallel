4141d4dbafa6210012336c2674f96bdcf95b82af
Jeremy Melvin <jmelvin@login1.stampede2.tacc.utexas.edu>
2019-04-04 12:08:26 -0500

    Bash script executing python script with different args in parallel.

---
+++ test.py
@@ -0,0 +1,4 @@
+import sys
+
+print '1st Command line argument: ', sys.argv[1]
+print '2nd Command line argument: ', sys.argv[2]
---
+++ testBash.sh
@@ -0,0 +1,5 @@
+for i in {1..5} 
+do 
+  python test.py $i $((i+1)) &  
+done 
+wait
